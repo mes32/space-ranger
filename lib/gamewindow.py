efdff5c691e1e3ea897b3617ae36f083c65997aa
mes32 <stockman.mike@gmail.com>
2015-03-10 14:11:16 -0400

    Added comments to gamewindow.py module

--- lib/gamewindow.py
+++ lib/gamewindow.py
@@ -1,13 +1,21 @@
+"""Module corresponding to the main game window
+
+"""
+
 import pygame
 
+WINDOW_WIDTH = 600   # Height of the game window in pixels
+WINDOW_HEIGHT = 700  # Width of the game window in pixels
 
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 700
-
-BACKGROUND_COLOR = (0, 0, 0)
-FRAMES_PER_SEC = 40
+BACKGROUND_COLOR = (0, 0, 0)  # Black background color for game window
+FRAMES_PER_SEC = 40           # Rate at which game window is repainted (40x per second)
 
 def initWindowSurface():
-    pygame.display.set_caption('Space Ranger')
+    """Initializes the game window.
+
+    Sets the window title bar caption, turns off the mouse cursor, and sets 
+    dimensions.
+    """
+    pygame.display.set_caption('space-ranger')
     pygame.mouse.set_visible(False)
     return pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
