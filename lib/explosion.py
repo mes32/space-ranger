5303f1712af69e81a9b8e09309868e780b8b33d0
mes32 <stockman.mike@gmail.com>
2015-03-10 13:59:21 -0400

    Added comments to explosion.py module

--- lib/explosion.py
+++ lib/explosion.py
@@ -1,9 +1,13 @@
+"""Module corresponding to exploding astroid animations
+
+"""
 
 import pygame
 
 EXPLOSION_IMAGE = pygame.image.load('./resources/images/astroidExplosion.png')
 
 class Explosion:
+    """Represents an exploding astroid"""
 
     def __init__(self, astroid):
         self.rect = astroid.getRect()
@@ -13,19 +17,22 @@ class Explosion:
         self.stage = 1
                        
     def move(self):
+        """Move explosion and evolve animation"""
         self.rect.move_ip(0, self.speed)
 
+        # Animate explosion expanding for a few frames/stages
         self.stage += 1
         oldSize = self.size
         self.size = int(oldSize*1.25)
         self.surface = pygame.transform.scale(EXPLOSION_IMAGE, (self.size, self.size))
         self.rect.move_ip(int((oldSize - self.size)/2), int((oldSize - self.size)/2))
 
-
     def draw(self, windowSurface):
+        """Draws the explosion on the screen"""
         windowSurface.blit(self.surface, self.rect)
 
     def isOffScreen(self):
+        """Returns True if the explosion is past frame 4 of its animation"""
         if self.stage > 4:
             return True
         else:
