c3e8af81c7562ebf6839386b9f065542ec797cd3
mes32 <stockman.mike@gmail.com>
2015-03-10 14:04:12 -0400

    Added comments to gametext.py module

--- lib/gametext.py
+++ lib/gametext.py
@@ -1,24 +1,31 @@
+"""Module corresponding to text writen on screen during the game
+
+"""
+
 import pygame
-#import sys
-#from pygame.locals import *
-#sys.path.append('./lib')
 from gamewindow import *
 
 TEXT_COLOR = (155, 155, 255)
 
 def initFont():
-
+    """Initializes the game FONT for later use.
+    
+    Requires that pygame was previously initialized
+    """
     global FONT
     FONT = pygame.font.SysFont(None, 32)
-    return pygame.font.SysFont(None, 32)
+
+    return FONT
 
 def draw(text, surface, x, y):
+    """Draws text on the game window at the x-y coordinates"""
     textobj = FONT.render(text, 1, TEXT_COLOR)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
 def drawCenter(text, surface, y):
+    """Draws text on the game window with centered x-alignment and y height"""
     textobj = FONT.render(text, 1, TEXT_COLOR)
     textrect = textobj.get_rect()
     textrect.centery = y
